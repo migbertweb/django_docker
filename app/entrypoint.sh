2f18d2a338d193db127902a4f066de995dfba5cc
migbertweb <migbertyanez@disroot.org>
2022-10-02 18:51:12 -0400

    añadiendo ramas para probar mariadb y mysql

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
+if [ "$DATABASE" = "mariadb" ]
 then
-    echo "Waiting for postgres..."
+    echo "Waiting for Mariadb o mysql..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
 
-    echo "PostgreSQL started"
+    echo "MariaDB o MySql started"
 fi
 
  python manage.py flush --no-input
